8b9009ec9a08b70c16d85639876f563f88ae2016
Naeem-fin <naeem.finsoft@gmail.com>
2026-05-22 01:12:49 +0600

    change: authorization

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -6,52 +6,54 @@ import type { TRole } from "../types";
 const authMiddleware = (...roles: TRole[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-        //todo verify token
-    //* token present in header
-    const token = req.headers.authorization;
-    //todo check if token is present
-    if (!token) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized: No token provided",
-      });
-    }
+      //todo verify token
+      //* token present in header
+      const token = req.headers.authorization;
+      //todo check if token is present
+      if (!token) {
+        return res.status(401).json({
+          success: false,
+          message: "Unauthorized: No token provided",
+        });
+      }
 
-    //todo verify the user with the token and db
+      //todo verify the user with the token and db
 
-    const decoded = jwt.verify(
-      token as string,
-      config.jwtAccessSecret as string,
-    ) as JwtPayload;
+      const decoded = jwt.verify(
+        token as string,
+        config.jwtAccessSecret as string,
+      ) as JwtPayload;
 
-    const userData = await pool.query(
-      `
+      const userData = await pool.query(
+        `
             SELECT * FROM users WHERE email = $1 and id = $2
         `,
-      [decoded.email, decoded.id],
-    );
+        [decoded.email, decoded.id],
+      );
 
-    if (userData.rows.length === 0) {
-      return res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
-    }
-    //todo add role based access control
-    if (roles.length && !roles.includes(userData.rows[0].role)) {
-      return res.status(403).json({
-        success: false,
-        message: "Forbidden:U are not allowed to access here",
-      });
-    }
-    req.user = userData.rows[0];
-    next();
+      if (userData.rows.length === 0) {
+        return res.status(404).json({
+          success: false,
+          message: "User not found",
+        });
+      }
+      console.log("Token Role:", userData.rows[0].role);
+      console.log("Allowed Roles:", roles);
+      //todo add role based access control
+      if (roles.length && !roles.includes(userData.rows[0].role)) {
+        return res.status(403).json({
+          success: false,
+          message: "Forbidden:U are not allowed to access here",
+        });
+      }
+      req.user = userData.rows[0];
+      next();
     } catch (error: any) {
-        return res.status(401).json({
+      return res.status(401).json({
         success: false,
         message: "Unauthorized access",
       });
-    }  
+    }
   };
 };
 export default authMiddleware;
